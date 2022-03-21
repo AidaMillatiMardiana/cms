ca06af656210e4dddc1ebb54652ca5c40f5b9b3a
AidaMillatiMardiana <aidadiana2702@gmail.com>
2022-03-21 17:37:17 +0700

    Praktikum 2: Add login as Administrator

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('home');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
